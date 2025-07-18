c92f57cc102dfc41a7e4285e9c8bb7181359e204
Nevan Catoy <93510179+ncvyn@users.noreply.github.com>
2025-07-18 13:25:40 +0800

    Replace with read_texts.rs example

--- src/parse.rs
+++ src/parse.rs
@@ -2,45 +2,24 @@ use quick_xml::events::Event;
 use quick_xml::reader::Reader;
 
 pub fn parse_xml() {
-    let xml = r#"<tag1 att1 = "test">
-                <tag2><!--Test comment-->Test</tag2>
-                <tag2>Test 2</tag2>
-             </tag1>"#;
+    let xml = "<tag1>text1</tag1><tag1>text2</tag1>\
+               <tag1>text3</tag1><tag1><tag2>text4</tag2></tag1>";
+
     let mut reader = Reader::from_str(xml);
     reader.config_mut().trim_text(true);
 
-    let mut count = 0;
-    let mut txt = Vec::new();
-    let mut buf = Vec::new();
-
     loop {
-        match reader.read_event_into(&mut buf) {
-            Err(e) => panic!("Error at position {}: {:?}", reader.error_position(), e),
-
+        match reader.read_event() {
+            Ok(Event::Start(e)) if e.name().as_ref() == b"tag2" => {
+                // read_text_into for buffered readers not implemented
+                let txt = reader
+                    .read_text(e.name())
+                    .expect("Cannot decode text value");
+                println!("{:?}", txt);
+            }
             Ok(Event::Eof) => break,
-
-            Ok(Event::Start(e)) => match e.name().as_ref() {
-                b"tag1" => println!(
-                    "attributes values: {:?}",
-                    e.attributes().map(|a| a.unwrap().value).collect::<Vec<_>>()
-                ),
-
-                b"tag2" => {
-                    count += 1;
-                    // clippy don't kill me
-                }
-
-                _ => (),
-            },
-
-            Ok(Event::Text(e)) => txt.push(e.into_owned()),
-
+            Err(e) => panic!("Error at position {}: {:?}", reader.error_position(), e),
             _ => (),
         }
-
-        buf.clear();
     }
-
-    println!("Count: {}", count);
-    println!("Text: {:?}", txt);
 }
